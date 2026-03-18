011ea9cc91a69455cf618f804d55a9ba1065469b
Airat <airatchigashov@gmail.com>
2026-03-18 11:45:20 +0300

    create protectAdmin

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+export const protectAdmin = (req, res, next) => {
+   const token = req.cookies.token; // Берем токен из кук
+
+   if (!token) {
+      return res.status(401).json({ message: 'Нет авторизации, доступ запрещен' });
+   }
+
+   try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Проверяем роль (мы создавали ENUM в базе: 'Админ', 'Пользователь')
+      if (decoded.role !== 'Админ') {
+         return res.status(403).json({ message: 'Недостаточно прав. Только для админов' });
+      }
+
+      req.user = decoded;
+      next();
+   } catch (error) {
+      res.status(401).json({ message: 'Неверный токен' });
+   }
+};
