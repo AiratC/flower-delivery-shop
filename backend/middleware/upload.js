1998c8e6c53a8a64c2492f98c31be7e03d204474
Airat <airatchigashov@gmail.com>
2026-03-22 18:15:35 +0300

    create upload.js multer storage

---
+++ backend/middleware/upload.js
@@ -0,0 +1,27 @@
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
+const storage = multer.diskStorage({
+   destination: (req, file, cb) => {
+      // Определяем папку в зависимости от того, что загружаем
+      let subFolder = req.originalUrl.includes('addons') ? 'addons' : 'flowers';
+      const dir = `./uploads/${subFolder}`;
+
+      if(!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+      cb(null, dir);
+   },
+   filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.round() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+   }
+});
+
+export const upload = multer({
+   storage,
+   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+   fileFilter: (req, file, cb) => {
+      if(file.mimetype.startsWith('image/')) cb(null, true);
+      else cb(new Error('Только images'), false);
+   }
+})
