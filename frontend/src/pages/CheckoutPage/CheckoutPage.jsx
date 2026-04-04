9d634ee6d3e7b613a713b41920da8d5d1b935a2d
Airat <airatchigashov@gmail.com>
2026-04-04 14:53:45 +0300

    create section delivery method

--- frontend/src/pages/CheckoutPage/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage/CheckoutPage.jsx
@@ -26,16 +26,76 @@ const CheckoutPage = () => {
 
    // Универсальный обработчик для всех типов инпутов
    const handleChange = (e) => {
-      const { name, value, type, checked } = e.target.value;
+      const { name, value, type, checked } = e.target;
+      console.log({ name, value, type, checked })
       setFormData(prevData => (
-         { ...prevData, [name] : type === 'checkbox' ? checked : value }
+         { ...prevData, [name]: type === 'checkbox' ? checked : value }
       ));
    };
 
-   
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      // Формируем итоговый объект для отправки в базу (Orders + Order_Items);
+      const finalOrder = {
+         ...formData,
+         totalPrice: totalPrice,
+         items: items.map((item) => ({
+            item_id: item.item_id,
+            item_type: item.item_type,
+            selected_size: item.selected_size,
+            quantity: item.quantity,
+            price_at_purchase: item.price
+         }))
+      };
+
+      console.log(`Готовый объект для backend: `, finalOrder)
+   };
+
+   if (items.length === 0) return <div className={`container`}>Корзина пуста</div>
    return (
-      <div>
-         CheckoutPage
+      <div className={`container ${styles.checkoutContainer}`}>
+         <h1 className={styles.mainTitle}>Оформление заказа</h1>
+
+         <form onSubmit={handleSubmit} className={`${styles.contentGrid}`}>
+
+            <div className={styles.formsColumn}>
+
+               {/* 1. Спопсоб доставки */}
+               <section className={styles.sectionCard}>
+                  <h3 className={styles.sectionHeader}>Способ доставки</h3>
+                  <div className={styles.radioGroupVertical}>
+                     <label htmlFor="delivery_method" className={styles.radioLabel}>
+                        <input
+                           type="radio"
+                           name='delivery_method'
+                           id='delivery_method'
+                           value={`Доставка по Владивостоку`}
+                           checked={formData.delivery_method === 'Доставка по Владивостоку'}
+                           onChange={handleChange}
+                        />
+                        <div className={styles.radioText}>
+                           <strong>Доставка по Владивостоку</strong>
+                        </div>
+                     </label>
+                     <label htmlFor="self" className={styles.radioLabel}>
+                        <input
+                           type="radio"
+                           name='delivery_method'
+                           id='self'
+                           value={`Самовывоз`}
+                           checked={formData.delivery_method === 'Самовывоз'}
+                           onChange={handleChange}
+                        />
+                        <div className={styles.radioText}>
+                           <strong>Самовывоз</strong>
+                           <span>г. Владивосток, ул. Пушкинская, 17 А</span>
+                        </div>
+                     </label>
+                  </div>
+               </section>
+            </div>
+         </form>
       </div>
    )
 }
